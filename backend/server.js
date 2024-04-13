6f4ddab206f9ea8e1d24f5b9e47f5077b4217b24
Naman Pandey <namanpandey836@gmail.com>
2024-04-14 00:21:59 +0530

    My commit

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,9 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.log(err));
 
+    app.get("/",(req,res)=>{
+        res.send("Hello i am Backend")
+        })
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
